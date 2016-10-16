373d9998a074176260bb68d32adc2ba0815180dd
Nyah Check <check.nyah@gmail.com>
2016-10-16 21:40:40 +0100

    --amend

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -10,17 +10,19 @@ import "errors"
 // Fibonacci: Returns the Nth fibonacci sequence for any N
 // N must be greater than 0
 // Fibonacci(N)
-func fibonacci (N int) ([]int, error) {
-  sequence := make([]int, 0, 5)
-  if n < 1 {
-    return sequence, errors.New("Invalid N value for Sequence")
-  } else {
-    j := N - 1
-    k := N
-    for i := 0; i < N ; i++ {
-      sequence = append(score, j++ + k++)
-    }
-  }
+func fibonacci(N int) ([]int, error) {
+	sequence := make([]int, 0, 5)
+	if N < 1 {
+		return sequence, errors.New("Invalid N value for Sequence")
+	} else {
+		j := N - 1
+		k := N
+		for i := 0; i < N; i++ {
+			sequence = append(sequence, j+k)
+			j++
+			k++
+		}
+	}
 
-  return sequence, nil
+	return sequence, nil
 }
